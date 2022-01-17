ba4ade7fc7671c3b037480446482d5888309adc7
Filipa Ramos <fimamora@gmail.com>
2022-01-17 00:41:03 +0000

    Changing parameters

--- train.py
+++ train.py
@@ -138,15 +138,28 @@ def train(config):
 
         train_gen.on_epoch_end()
 
+class CustomSchedule(tf.keras.optimizers.schedules.LearningRateSchedule):
+   def __init__(self, d_model, warmup_steps=4000):
+      super(CustomSchedule, self).__init__()
+      self.d_model = d_model
+      self.d_model = tf.cast(self.d_model, tf.float32)
+      self.warmup_steps = warmup_steps
+
+   def __call__(self, step):
+      arg1 = tf.math.rsqrt(step)
+      arg2 = step * (self.warmup_steps ** -1.5)
+      return tf.math.rsqrt(self.d_model) * tf.math.minimum(arg1, arg2)
+
 def train2D(config):
     # Prepare variables
     run_path, cfg, vocab_size, max_length, caption_model = prep_train(config)
 
-    lr_schedule = tf.keras.optimizers.schedules.ExponentialDecay(
+    '''lr_schedule = tf.keras.optimizers.schedules.ExponentialDecay(
                             initial_learning_rate=1e-4,
-                            decay_steps=10000,
-                            decay_rate=0.9
-    )
+                            decay_steps=4000,
+                            decay_rate=1.5
+    )'''
+    lr_schedule = CustomSchedule(int(cfg['EMBED_DIM']))
     optimizer = tf.keras.optimizers.Adam(lr_schedule, beta_1=0.9, beta_2=0.98,
                                     epsilon=1e-9)
     loss_object = SparseCategoricalCrossentropy(from_logits=True, reduction='none')
